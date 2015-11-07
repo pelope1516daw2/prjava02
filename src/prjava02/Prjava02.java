7092dec9c01053eb57c18723486a6b5b9724e377
pelope1516daw2 <pelope1516daw2@gmail.com>
2015-11-07 13:16:26 +0100

    version 0.0 del projecte prjava02

---
+++ src/prjava02/Prjava02.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prjava02;
+import java.net.*;
+import java.io.*;
+/**
+ *
+ * @author ­­> pelope1516
+
+ */
+public class Prjava02 {
+   
+    public static void main(String[] args) {
+        System.out.println("versió0.1 del projecte prjava02"); 
+        try {
+            InetAddress adreça = InetAddress.getLocalHost();
+            String hostname = adreça.getHostName();
+            System.out.println("hostname="+hostname);
+            System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
+            System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
+            System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
+            System.out.println("Versió OS: " + System.getProperty("os.version"));
+        }
+        catch (IOException e) {
+        }  
+    }
+}
